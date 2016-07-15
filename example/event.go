d0aebd21faeb0c2ac8ab6f3104f3699f78e29929
diego <diegogub@gmail.com>
2016-07-15 17:00:10 -0300

    make exaple basic functional

--- example/event.go
+++ example/event.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"errors"
 	"github.com/diegogub/esgo"
 	"time"
 )
 
+var (
+	InvalidExampleID = errors.New("invalid id for stream")
+)
+
 type ExampleEventDone struct {
 	esgo.BaseEvent
+	ID          string    `json:"id"`
 	ExampleData string    `json:"example"`
 	Date        time.Time `json:"date"`
 }
 
 func (te ExampleEventDone) GetStreamGroup() string {
-	return "testing"
+	return "examples"
 }
 
 func (te ExampleEventDone) GetUserID() string {
@@ -21,4 +27,17 @@ func (te ExampleEventDone) GetUserID() string {
 
 func (te ExampleEventDone) MustCreate() bool {
 	return false
+}
+
+func (te ExampleEventDone) CheckUniqueValue() []string {
+	return []string{}
+}
+
+func (ex *ExampleEventDone) Build() error {
+	var e ExampleEventDone
+	if e.ID == "" {
+		return InvalidExampleID
+	}
+	e.SetStream(e.ID)
+	return nil
 }
--- example/main.go
+++ example/main.go
@@ -10,13 +10,35 @@ var r *esgo.CommandRouter
 type ExampleCmdHandler struct {
 }
 
-func (c ExampleCmdHandler) Deal(cmd *esgo.Command) (esgo.Eventer, esgo.CommandResult) {
+func (e ExampleCmdHandler) Commands() []string {
+	return []string{
+		"MakeExaple",
+	}
+}
+
+func (c ExampleCmdHandler) Deal(cmd *esgo.Command) (esgo.Eventer, *esgo.CommandResult) {
+
+	var e esgo.Eventer
+	var res *esgo.CommandResult
+
+	switch cmd.Name {
+	case "":
+		event := &ExampleEventDone{}
+		res = esgo.NewCommandResult(event)
+		cmd.SetEvent(event)
+		e = event
+	}
+
+	return e, res
 }
 
 func main() {
 	es := arango.ArangoES{}
 	arango.Init("")
+
 	r = esgo.NewCommandRouter(es)
 
-	r.AddCommandHandler()
+	//Add Example commands handler
+	r.AddCommandHandler(ExampleCmdHandler{}, ExampleCmdHandler{}.Commands()...)
+
 }
